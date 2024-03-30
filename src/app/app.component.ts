221d7f518aec90c21141141dd7c90efa56440dd2
AlexChris <a.chrisafidis@gmail.com>
2024-03-30 15:33:26 +0200

    update read

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,21 @@ import {PersonTableComponent} from "./components/person-table/person-table.compo
 export class AppComponent {
   name = "Alexandros";
 
+  person0=
+    {
+    givenName: "Alexandros",
+    surName: "Chrisafidis",
+    age: 0x32,
+    email: "cf5@aueb.gr",
+    address:"Athens, Greece"
+  }
+  person1=
+    {
+      givenName: "John",
+      surName: "Giannakis",
+      age: 0x32,
+      email: "cf3@aueb.gr",
+      address:"London, England"
+    }
 
 }
