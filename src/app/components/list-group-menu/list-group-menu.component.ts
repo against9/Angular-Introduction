419925da9a79b7ebe8716ac03a88a7f0a29bf25c
AlexChris <a.chrisafidis@gmail.com>
2024-04-01 20:05:20 +0300

    funcy app menu

---
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+import {MenuItem} from "../../shared/interfaces/menu-item";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+
+@Component({
+  selector: 'app-list-group-menu',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './list-group-menu.component.html',
+  styleUrl: './list-group-menu.component.css'
+})
+export class ListGroupMenuComponent {
+  menu: MenuItem[] = [
+    {text: "Component Input Example", routerLink: "component-input-example"},
+    {text: "@For Directive Example", routerLink: "for-directive-example"},
+    {text: "Event Bind Example", routerLink: "event-bind-example"}
+  ]
+
+}
